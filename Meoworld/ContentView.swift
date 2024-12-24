583f9083622b920e852805d14177dde806df881b
UUNEO <uuneo@qq.com>
2024-12-24 09:25:05 +0800

    Initial Commit

---
+++ Meoworld/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  File name:     ContentView.swift
+//  Author:        Copyright (c) 2024 QingHe. All rights reserved.
+//  Blog  :        https://uuneo.com
+//  E-mail:        to@uuneo.com
+
+//  Description:
+
+//  History:
+//  Created by uuneo on 2024/12/24.
+	
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Meoworld/MeoworldApp.swift
@@ -0,0 +1,22 @@
+//
+//  File name:     MeoworldApp.swift
+//  Author:        Copyright (c) 2024 QingHe. All rights reserved.
+//  Blog  :        https://uuneo.com
+//  E-mail:        to@uuneo.com
+
+//  Description:
+
+//  History:
+//  Created by uuneo on 2024/12/24.
+	
+
+import SwiftUI
+
+@main
+struct MeoworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
